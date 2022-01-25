3b123bd9338b86e0a48e316a36ea2cfc32f3c9a4
oh-sumin <osumin740@gmail.com>
2022-01-25 10:58:03 +0900

    Rename login to Login

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/login';
+import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import MyPage from './pages/MyPage/MyPage';
 import Payment from './pages/Payment/Payment';
