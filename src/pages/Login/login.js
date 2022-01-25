479c409621df23d49a7a213f9e5d951d74985c3d
oh-sumin <osumin740@gmail.com>
2022-01-25 10:37:29 +0900

    Add login page

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,7 +1,53 @@
 import React from 'react';
 
 const Login = () => {
-  return <div />;
+  return (
+    <>
+      <div className="loginHeaderWapper">
+        <div className="loginHead">
+          <div className="loginHeadTop">
+            <img
+              className="loginHeadTop backButton"
+              src="images/backBotton.png"
+              alt="뒤로가기"
+            ></img>
+          </div>
+          <div className="loginHeadBottom">
+            <h1 className="loginHeadBottom title">로그인</h1>
+          </div>
+        </div>
+      </div>
+      <div className="loginBodyWapper">
+        <div className="loginBodyWapper loginWapper">
+          <header className="loginBodyWapper loginWapper header">
+            <h2 className="loginBodyWapper loginWapper header title">
+              <strong>어서오세요</strong>
+              <span>닥터마틴에 오신걸 환영합니다.</span>
+            </h2>
+          </header>
+          <div className="loginBody">
+            <div className="loginBody loginColumn">
+              <h3 className="formTitle">로그인</h3>
+              <from className="loginFrom">
+                <div className="loginInput">
+                  <label>아이디</label>
+                  <div className="inputContainer">
+                    <input name="userid" type="text"></input>
+                  </div>
+                </div>
+                <div className="loginInput">
+                  <label>비밀번호</label>
+                  <div className="inputContainer">
+                    <input name="userpw" type="password"></input>
+                  </div>
+                </div>
+              </from>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
