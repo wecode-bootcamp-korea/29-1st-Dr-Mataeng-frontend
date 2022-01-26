57217821a10ffa88cc6dfaaa13af72dde9819cf1
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-01-26 16:45:36 +0900

    [29기 유재민] 마이페이지 1차 리뷰 반영

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -4,71 +4,71 @@ import './MyPage.scss';
 
 const Mypage = () => {
   return (
-    <div className="MypageWrap">
-      <div className="imgUploadWrap">
+    <main className="Mypage">
+      <section className="imgUploadWrap">
         <img
-          alt="defaultImg"
-          className="imgUploadDefault"
+          alt="dr mataeng logo"
+          className="defaultImg"
           src="/images/mypage/logo@3x.png"
         />
         <div className="imgUploadBtn">
           <img
-            alt="buttonImg"
-            className="imgUploadBtnImg"
+            alt="photo icon"
+            className="uploadBtnImg"
             src="/images/mypage/img_profile.png"
           />
         </div>
-      </div>
-      <div className="myPageInfoWrap">
-        <div className="myPageInfoWelcome">
+      </section>
+      <section className="infoWrap">
+        <div className="infoGreetingsWrap">
           <button type="button" className="gearBtn">
             <img
-              alt="gearBtnImg"
+              alt="gear icon"
               className="gearBtnImg"
               src="/images/mypage/btn_mypageSetting.jpeg"
             />
           </button>
-          <span className="myPageUserName">
+          <span className="userName">
             <strong>유재민님,</strong>
           </span>
-          <span className="myPageGreetings">환영합니다.</span>
+          <span className="greetings">환영합니다.</span>
         </div>
-        <div className="MypageInformationLevel">
-          <p className="myPageLevel">DOCS FAMILY</p>
+        <div className="userRankWrap">
+          <p className="userRank">DOCS FAMILY</p>
         </div>
-        <div className="MypageSummaryItems">
-          {pointList.map(el => (
+        <div className="userPoingWrap">
+          {POINT_LIST.map(({ id, pointType, pointHistory, url }) => (
             <MypagePoints
-              key={el.id}
-              label={el.pointLabel}
-              point={el.holdingPoint}
-              url={el.url}
+              key={id}
+              label={pointType}
+              point={pointHistory}
+              url={url}
             />
           ))}
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
 // 상수 데이터 : 포인트
-const pointList = [
+const POINT_LIST = [
   {
     id: 1,
-    pointLabel: '포인트',
-    holdingPoint: 10000,
+    pointType: '포인트',
+    pointHistory: 10000,
     url: '/images/mypage/img_summary01.jpeg',
   },
   {
     id: 2,
-    pointLabel: '쿠폰',
-    holdingPoint: 1,
+    pointType: '쿠폰',
+    pointHistory: 1,
     url: '/images/mypage/img_summary02.jpeg',
   },
   {
     id: 3,
-    pointLabel: '미션',
-    holdingPoint: 4,
+    pointType: '미션',
+    pointHistory: 4,
     url: '/images/mypage/img_summary03.jpeg',
   },
 ];
--- src/pages/MyPage/MypagePoints.js
+++ src/pages/MyPage/MypagePoints.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const MypagePoints = ({ id, label, point, url }) => {
   return (
-    <div className="SummaryItemsWrap" key={id}>
-      <img alt="summaryIcon" className="summaryIcons" src={url} />
-      <span className="summaryIconName">{label}</span>
+    <div className="userPoingHistoryWrap" key={id}>
+      <img alt="point icon" className="pointIcon" src={url} />
+      <span className="pointIconName">{label}</span>
       <span className="holdingPoints">{point}</span>
     </div>
   );
