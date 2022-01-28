f7344bd8ec37a6fb8269f47dae75fc79925a1df6
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-01-28 15:57:10 +0900

    [29기 유재민] 헤더, 네비게이션 기능 구현

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,14 +7,38 @@ import './Header.scss';
 
 const Header = () => {
   const [navHandler, setnavHandler] = useState(false);
+  // const [inputValue, setInputValue] = useState('');
+  // const [searchData, setSearchData] = useState([]);
 
   const navDisplayHandler = () => {
     setnavHandler(!navHandler);
   };
 
+  // const inputValueRecord = event => {
+  //   setInputValue(event.target.value);
+  // };
+
+  // useEffect(() => {
+  //   fetch('/data/comment.json', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       name: inputValue,
+  //     }),
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setSearchData(...data);
+  //     });
+  // }, [inputValue]);
+
   return (
     <header className="Header">
-      {navHandler && <Nav navDisplayHandler={navDisplayHandler} />}
+      {navHandler && (
+        <Nav
+          navDisplayHandler={navDisplayHandler}
+          // inputValueRecord={inputValueRecord}
+        />
+      )}
       <div className="topMenuListWrap">
         <ul className="topMenuList">
           {TOP_BAR_MENU_LIST.map(({ id, url, buttonName }) => (
@@ -40,7 +64,11 @@ const Header = () => {
           src="/images/header/header-logo.svg"
         />
         <div className="searchAndIconWrap">
-          <input className="searchInput" placeholder="검색어를 입력해주세요." />
+          <input
+            className="searchInput"
+            placeholder="검색어를 입력해주세요."
+            // onChange={inputValueRecord}
+          />
           <div className="iconListWrap">
             {HEADER_ICON_LIST.map(({ id, btnClass, iconClass, url }) => (
               <button className={btnClass} key={id}>
