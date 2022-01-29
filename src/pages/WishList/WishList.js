a90fb9ace951e1ca04892d9ba6de3812807e7af2
oh-sumin <osumin740@gmail.com>
2022-01-29 11:12:00 +0900

    Add back button

--- src/pages/WishList/WishList.js
+++ src/pages/WishList/WishList.js
@@ -4,6 +4,18 @@ import Footer from '../../components/footer/footer';
 const WishList = () => {
   return (
     <>
+      <div className="loginHeaderWrapper">
+        <div className="loginHead">
+          <div className="loginHeadTop">
+            <button className="loginHeadTop backButton">
+              <img src="images/backBotton.png" alt="뒤로가기" />
+            </button>
+          </div>
+          <div className="loginHeadBottom">
+            <h1 className="loginHeadBottom title">로그인</h1>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
