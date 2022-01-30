bafd59e4aca345a2a000b9957f7410993c5cc8c4
oh-sumin <osumin740@gmail.com>
2022-01-30 10:08:35 +0900

    Add goToMain button

--- src/pages/WishList/Empty.js
+++ src/pages/WishList/Empty.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Empty = () => {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/Main');
+  };
+
   return (
     <>
       <div className="loginHeaderWrapper">
@@ -24,7 +30,7 @@ const Empty = () => {
             <div className="emptyCartComment">
               <span>장바구니가 비었습니다.</span>
             </div>
-            <button>
+            <button onClick={goToMain}>
               <span>쇼핑 계속하기</span>
             </button>
           </div>
