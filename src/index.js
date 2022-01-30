30dd03f2d25a5435a91dff569278efc4df7ae874
oh-sumin <osumin740@gmail.com>
2022-01-30 10:26:10 +0900

    Add variables.scss to index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import Router from './Router';
 import './styles/reset.scss';
 import './styles/common.scss';
+import './styles/variables.scss';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
