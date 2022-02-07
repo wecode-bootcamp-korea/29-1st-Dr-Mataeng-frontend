deb40db76699f8c46226c794acb76b0fddca6011
oh-sumin <osumin740@gmail.com>
2022-02-07 20:00:36 +0900

    Add fetch function

--- src/pages/WishList/ProductList.js
+++ src/pages/WishList/ProductList.js
@@ -5,20 +5,20 @@ function ProductList(props) {
     <li className="itemList">
       <div className="productContainer">
         <div className="productBox">
-          <img src={props.img} alt="productimage" />
+          <img src={props.image} alt="productimage" />
           <div className="productInfoBox">
             <div className="productInfo">
-              <h2>{props.name}</h2>
+              <h2>{props.productName}</h2>
               <ul className="productDetail">
                 <li>컬러 : {props.color} </li>
                 <li>사이즈 : {props.size}</li>
-                <li>수량 : {props.option}</li>
+                <li>수량 : {props.quantity}</li>
                 <li className="point">포인트 사용 불가</li>
               </ul>
             </div>
             <div className="productPrice">
               <span className="mark">₩</span>
-              <span className="price">{props.price * props.option}</span>
+              <span className="price">{props.price}</span>
             </div>
           </div>
         </div>
@@ -26,7 +26,7 @@ function ProductList(props) {
       <div className="productFooter">
         <div className="wishButton">
           <img alt="heartbutton" src="images/header/icon-heart.png" />
-          <span>200</span>
+          <span>{props.like}</span>
         </div>
         <div className="wishListButton">
           <button>위시리스트 담기</button>
--- src/pages/WishList/WishList.js
+++ src/pages/WishList/WishList.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './WishList.scss';
-import Cart from '../WishList/Cart';
-//import Empty from '../WishList/Empty';
 import Footer from '../../components/footer/footer';
+import { useState } from 'react/cjs/react.development';
+import ProductList from './ProductList';
 
 const WishList = () => {
+  const [cartList, setCartList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://10.58.4.82:8000/carts', {
+      method: 'GET',
+      headers: {
+        Authorization:
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1fQ.wJO6SJNZeBgZWe8KLTo2flSDaL0KdDOA_oBpObKiRCw',
+      },
+    })
+      .then(response => response.json())
+      .then(data => setCartList(data.result));
+  }, []);
+
   return (
     <>
       <div className="loginHeaderWrapper">
@@ -19,7 +33,77 @@ const WishList = () => {
           </div>
         </div>
       </div>
-      <Cart />
+      <div className="cart">
+        <div className="cart container">
+          <div className="containerRow">
+            <div className="controlCheckbox">
+              <div className="checkbox">
+                <input type="checkbox" />
+                <span>전체선택</span>
+              </div>
+              <div className="deleteButton">
+                <button>
+                  <img alt="trashcan" src="images/icon/trash-can.png" />
+                  <span>선택 삭제</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="cartBox">
+          <div className="cartBoxContainer">
+            <ul className="orderProductList">
+              {cartList.map(list => {
+                return (
+                  <ProductList
+                    key={list.product_id}
+                    productName={list.product_name}
+                    image={list.product_image}
+                    color={list.product_color}
+                    size={list.product_size}
+                    quantity={list.quantity}
+                    price={list.price}
+                    like={list.product_like}
+                  />
+                );
+              })}
+            </ul>
+          </div>
+          <div className="orderBoxContainer">
+            <div className="orderBox">
+              <div className="orderTitle">
+                <span>주문 예정 금액</span>
+              </div>
+              <div className="orderPriceBox">
+                <div className="orderPriceList">
+                  <dt className="orderPriceTitle">총 상품 금액</dt>
+                  <dd className="orderPrice">
+                    <span>₩</span>
+                    <span>3,000</span>
+                  </dd>
+                </div>
+                <div className="orderPriceList">
+                  <dt className="orderPriceTitle">할인 금액</dt>
+                  <dd className="orderPrice">
+                    <span>₩</span>
+                    <span>(-)0</span>
+                  </dd>
+                </div>
+                <div className="orderPriceList">
+                  <dt className="orderPriceTitletotal">총 결제 예정 금액</dt>
+                  <dd className="orderPrice">
+                    <span>₩</span>
+                    <span>3,000</span>
+                  </dd>
+                </div>
+              </div>
+            </div>
+            <div className="buyButton">
+              <button>구매하기</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
