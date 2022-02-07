463557f405ac4708d9c3b4776e7d9f672fb5aacb
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-02-07 22:29:51 +0900

    [29기 유재민] 헤더 추가 기능 구현 중

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Nav from './Nav';
 
 import './header.scss';
@@ -14,11 +14,12 @@ const Header = () => {
     setnavHandler(!navHandler);
   };
 
+  // Event : 헤더 검색어 받아오기
   const inputValueRecord = event => {
     setInputValue(event.target.value);
   };
 
-  // event3 : 검색어 입력 값이 0 보다 작거나 같은 경우 Link태그 이동 막기
+  // Event : 검색어 입력 값 조건에 따른 Link태그 이동 막기
   const preventLinkHandler = ({ id, event }) => {
     if (id === 1) {
       if (inputValue.length <= 0) event.preventDefault();
@@ -27,7 +28,21 @@ const Header = () => {
     }
   };
 
-  // 1. 데이터 필터 처리하여 받아오는 로직
+  // Event : 엔터 키 입력 시 페이지 이동 및 value 전달
+  const navigate = useNavigate();
+
+  const handleClick = event => {
+    if (event.key === 'Enter' && inputValue.length > 0) {
+      navigate('/Search', {
+        state: {
+          inputValue: inputValue,
+          productFilter: '여기에 백에서 전달받은 data를 집어넣어야 한닷!!!',
+        },
+      });
+    }
+  };
+
+  // Event : 필터 처리 된 데이터 받아오기
   // useEffect(() => {
   //   fetch('/data/comment.json', {
   //     method: 'POST',
@@ -78,6 +93,7 @@ const Header = () => {
             className="searchInput"
             placeholder="검색어를 입력해주세요."
             onChange={inputValueRecord}
+            onKeyPress={handleClick}
           />
           <div className="iconListWrap">
             {HEADER_ICON_LIST.map(
