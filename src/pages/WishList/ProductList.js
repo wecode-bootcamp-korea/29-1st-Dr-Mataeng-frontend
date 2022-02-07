e4b79c3f22ac4865bb611861ce6b205d9f5c115d
oh-sumin <osumin740@gmail.com>
2022-02-08 03:46:08 +0900

    Fix Sider

--- src/pages/WishList/ProductList.js
+++ src/pages/WishList/ProductList.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
 function ProductList(props) {
+  let { color, size, quantity, price, like, image, productName } = this.props;
   return (
     <li className="itemList">
       <div className="productContainer">
         <div className="productBox">
-          <img src={props.image} alt="productimage" />
+          <img src={image} alt="productimage" />
           <div className="productInfoBox">
             <div className="productInfo">
-              <h2>{props.productName}</h2>
+              <h2>{productName}</h2>
               <ul className="productDetail">
-                <li>컬러 : {props.color} </li>
-                <li>사이즈 : {props.size}</li>
-                <li>수량 : {props.quantity}</li>
+                <li>컬러 : {color} </li>
+                <li>사이즈 : {size}</li>
+                <li>수량 : {quantity}</li>
                 <li className="point">포인트 사용 불가</li>
               </ul>
             </div>
             <div className="productPrice">
               <span className="mark">₩</span>
-              <span className="price">{props.price}</span>
+              <span className="price">{price}</span>
             </div>
           </div>
         </div>
@@ -26,7 +27,7 @@ function ProductList(props) {
       <div className="productFooter">
         <div className="wishButton">
           <img alt="heartbutton" src="images/header/icon-heart.png" />
-          <span>{props.like}</span>
+          <span>{like}</span>
         </div>
         <div className="wishListButton">
           <button>위시리스트 담기</button>
