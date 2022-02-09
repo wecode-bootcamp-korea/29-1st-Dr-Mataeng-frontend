3aa65d4d142194a97bce028de31a5acfbd258931
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-02-09 14:24:01 +0900

    [29기 유재민] 마이페이지 주문 내역 기능 추가 중

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import MypagePoints from './MypagePoints';
+import { Link } from 'react-router-dom';
 import './MyPage.scss';
 
 const Mypage = () => {
   const [orderProduct, setOrderProduct] = useState(false);
+
+  const orderProductHandler = () => {
+    setOrderProduct(!orderProduct);
+  };
 
   return (
     <main className="Mypage">
@@ -51,36 +56,81 @@ const Mypage = () => {
           </div>
         </section>
       </main>
-      <div className={orderProduct ? 'orderListWrap' : 'orderListWrapShow'}>
-        <div className="orderItemWrap">
-          <img
-            alt="basket icon"
-            className="basketIcon"
-            src="/images/payment/icon-bag.png"
-          />
-          <span className="title">주문상품</span>
-        </div>
-        <ul className="orderProductListWrap">
-          <li className="orderProductList">
-            <div className="productTumWrap">
-              <img
-                alt="shoes thumbnail"
-                className="productTum"
-                src="/images/payment/tum-test.jpeg"
-              />
+      <section className="Payment">
+        <div className="contentsWrap">
+          <div className="orderProductInfoWrap">
+            <div className="expectedOrderPrice">
+              <span className="title">주문 예정 금액 (1item | ₩280,000)</span>
+              <button className="orderListBtn" onClick={orderProductHandler}>
+                <img
+                  alt="arrow icon"
+                  className="arrowIcon"
+                  src={
+                    orderProduct
+                      ? '/images/payment/icon-down-arrow-white.png'
+                      : '/images/payment/icon-up-arrow-white.png'
+                  }
+                />
+              </button>
             </div>
-            <div className="productInfoWrap">
-              <h2 className="productInfoTitle">아드리안</h2>
-              <ul className="productInfoOption">
-                <li className="optionList">컬러 : 블랙</li>
-                <li className="optionList">사이즈 : 220</li>
-                <li className="optionList">수량 : 1</li>
+            <div
+              className={orderProduct ? 'orderListWrap' : 'orderListWrapShow'}
+            >
+              <div className="orderItemWrap">
+                <img
+                  alt="basket icon"
+                  className="basketIcon"
+                  src="/images/payment/icon-bag.png"
+                />
+                <span className="title">주문상품</span>
+              </div>
+              <ul className="orderProductListWrap">
+                <li className="orderProductList">
+                  <div className="productTumWrap">
+                    <img
+                      alt="shoes thumbnail"
+                      className="productTum"
+                      src="/images/payment/tum-test.jpeg"
+                    />
+                  </div>
+                  <div className="productInfoWrap">
+                    <h2 className="productInfoTitle">아드리안</h2>
+                    <ul className="productInfoOption">
+                      <li className="optionList">컬러 : 블랙</li>
+                      <li className="optionList">사이즈 : 220</li>
+                      <li className="optionList">수량 : 1</li>
+                    </ul>
+                    <span className="productInfoPrice">280000</span>
+                  </div>
+                </li>
               </ul>
-              <span className="productInfoPrice">280000</span>
             </div>
-          </li>
-        </ul>
-      </div>
+          </div>
+          <div className="orderPriceWrap">
+            <article className="orderPriceStickyWrap">
+              <span className="orderPriceTitle">주문 예정 금액</span>
+              <ul className="orderPriceListWrap">
+                <li className="orderPriceList">
+                  <span className="title">총 상품 금액</span>
+                  <span className="price">280,000</span>
+                </li>
+                <li className="orderPriceList">
+                  <span className="title">배송비</span>
+                  <span className="price">(+)0</span>
+                </li>
+                <li className="orderPriceList">
+                  <span className="title">할인 금액</span>
+                  <span className="price">(-)0</span>
+                </li>
+                <li className="orderPriceList">
+                  <span className="title titleAccent">총 결제 예정 금액</span>
+                  <span className="price priceAccent">280,000</span>
+                </li>
+              </ul>
+            </article>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
