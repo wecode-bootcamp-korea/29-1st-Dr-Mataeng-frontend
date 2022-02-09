b7a4c50ef5c363f679a5dfcd0cb8790174f1e424
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-02-09 13:27:04 +0900

    [29기 유재민] 마이페이지 주문 내역 기능 추가 중

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,52 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MypagePoints from './MypagePoints';
 import './MyPage.scss';
 
 const Mypage = () => {
+  const [orderProduct, setOrderProduct] = useState(false);
+
   return (
     <main className="Mypage">
-      <section className="imgUploadWrap">
-        <img
-          alt="dr mataeng logo"
-          className="defaultImg"
-          src="/images/mypage/logo@3x.png"
-        />
-        <div className="imgUploadBtn">
+      <main className="MypagePosition">
+        <section className="imgUploadWrap">
           <img
-            alt="photo icon"
-            className="uploadBtnImg"
-            src="/images/mypage/img_profile.png"
+            alt="dr mataeng logo"
+            className="defaultImg"
+            src="/images/mypage/logo@3x.png"
           />
-        </div>
-      </section>
-      <section className="infoWrap">
-        <div className="infoGreetingsWrap">
-          <button type="button" className="gearBtn">
+          <div className="imgUploadBtn">
             <img
-              alt="gear icon"
-              className="gearBtnImg"
-              src="/images/mypage/btn_mypageSetting.jpeg"
+              alt="photo icon"
+              className="uploadBtnImg"
+              src="/images/mypage/img_profile.png"
             />
-          </button>
-          <span className="userName">
-            <strong>유재민님,</strong>
-          </span>
-          <span className="greetings">환영합니다.</span>
+          </div>
+        </section>
+        <section className="infoWrap">
+          <div className="infoGreetingsWrap">
+            <button type="button" className="gearBtn">
+              <img
+                alt="gear icon"
+                className="gearBtnImg"
+                src="/images/mypage/btn_mypageSetting.jpeg"
+              />
+            </button>
+            <span className="userName">
+              <strong>유재민님,</strong>
+            </span>
+            <span className="greetings">환영합니다.</span>
+          </div>
+          <div className="userRankWrap">
+            <p className="userRank">DOCS FAMILY</p>
+          </div>
+          <div className="userPoingWrap">
+            {POINT_LIST.map(({ id, pointType, pointHistory, url }) => (
+              <MypagePoints
+                key={id}
+                label={pointType}
+                point={pointHistory}
+                url={url}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
+      <div className={orderProduct ? 'orderListWrap' : 'orderListWrapShow'}>
+        <div className="orderItemWrap">
+          <img
+            alt="basket icon"
+            className="basketIcon"
+            src="/images/payment/icon-bag.png"
+          />
+          <span className="title">주문상품</span>
         </div>
-        <div className="userRankWrap">
-          <p className="userRank">DOCS FAMILY</p>
-        </div>
-        <div className="userPoingWrap">
-          {POINT_LIST.map(({ id, pointType, pointHistory, url }) => (
-            <MypagePoints
-              key={id}
-              label={pointType}
-              point={pointHistory}
-              url={url}
-            />
-          ))}
-        </div>
-      </section>
+        <ul className="orderProductListWrap">
+          <li className="orderProductList">
+            <div className="productTumWrap">
+              <img
+                alt="shoes thumbnail"
+                className="productTum"
+                src="/images/payment/tum-test.jpeg"
+              />
+            </div>
+            <div className="productInfoWrap">
+              <h2 className="productInfoTitle">아드리안</h2>
+              <ul className="productInfoOption">
+                <li className="optionList">컬러 : 블랙</li>
+                <li className="optionList">사이즈 : 220</li>
+                <li className="optionList">수량 : 1</li>
+              </ul>
+              <span className="productInfoPrice">280000</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 };
