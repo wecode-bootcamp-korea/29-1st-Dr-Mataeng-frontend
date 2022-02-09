5f532b55fd63da525ed37f84397710db4cf4a2db
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-02-09 15:03:19 +0900

    [29기 유재민] 결제 페이지 마크업

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Payment.scss';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const Payment = () => {
   const [orderProduct, setOrderProduct] = useState(true);
