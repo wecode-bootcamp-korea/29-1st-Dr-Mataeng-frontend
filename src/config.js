00370027083b2a595f631b209298f1b5b05ce7a0
oh-sumin <osumin740@gmail.com>
2022-02-10 14:22:01 +0900

    Fix sider

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
-const BASE_URL = "http://10.58.6.159:8000"
+const BASE_URL = 'http://10.58.6.159:8000';
 
 const END_POINT = {
-  signIn = `${BASE_URL}/users/signin`,
-  signUp =`${BASE_URL}/users/user`,
-  myPage = `${BASE_URL}/users/user`,
-  productDetail = `${BASE_URL}/products`,
-  productList = `${BASE_URL}/products`,
-  search = `${BASE_URL}/products`,
-  wishList = `${BASE_URL}/carts`,
-  payment = `${BASE_URL}/orders`,
-}
+  signIn: `${BASE_URL}/users/signin`,
+  signUp: `${BASE_URL}/users/user`,
+  myPage: `${BASE_URL}/users/user`,
+  productDetail: `${BASE_URL}/products`,
+  productList: `${BASE_URL}/products`,
+  search: `${BASE_URL}/products`,
+  wishList: `${BASE_URL}/carts`,
+  payment: `${BASE_URL}/orders`,
+};
